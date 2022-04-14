47e885e5e4ae672f0e855c87cde57fff554750ed
Arthur Koba <caprimores@gmail.com>
2022-04-14 23:58:41 +0300

    DarkMode LocalStorage

--- frontend/static/dist/darkModeSwaper.js
+++ frontend/static/dist/darkModeSwaper.js
@@ -1,16 +1,32 @@
 class DarkModeSwaper {
   constructor () {
     this._swapButton = document.querySelector('.dark-mode-swaper i')
+    // this.darkMode = document.body.classList.contains("dark-mode")
+
+    this._initDarkMode()
     this._swapButton.parentNode
-                  .addEventListener('click', this.swap.bind(this))
+                  .addEventListener('click', this.darkModeHandler.bind(this))
+
+  }
+  _initDarkMode () {
+    this.darkMode = JSON.parse(localStorage.getItem('darkMode'))
+    if (this.darkMode === null) {
+      this.darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches
+      localStorage.setItem('darkMode', this.darkMode)
+    }
+    if (this.darkMode !== document.body.classList.contains('dark-mode')) { this._swap() }
   }
 
-
-  swap () {
+  _swap () {
     document.body.classList.toggle("dark-mode")
     this._swapButton.classList.toggle("fa-sun")
     this._swapButton.classList.toggle("fa-moon")
-    
+  }
+
+  darkModeHandler() {
+    this._swap()
+    this.darkMode = !this.darkMode
+    localStorage.setItem('darkMode', this.darkMode)
   }
 }
 
