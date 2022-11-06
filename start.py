e8826ba27cbc5d5222cd718db6352294b84f3bd8
Arthur Koba <caprimores@gmail.com>
2022-11-06 06:51:53 +0300

    refactor start.py

--- start.py
+++ start.py
@@ -1,19 +1,16 @@
 import uvicorn
 from backend.settings import get_config
 
+
 def start() -> None:
     config = get_config()
-    print("Starting server...")
-    try:
-        uvicorn.run(
-            app="backend:app",
-            port=config.getint('Server', 'port'),
-            log_level=config.get('Server', 'log_level'),
-            reload_includes=["*.html", '*.js', '*.css'],
-            reload=True, debug=True
-        )
-    except KeyboardInterrupt:
-        print("Server closed!")
+    uvicorn.run(
+        app="backend:app",
+        port=config.getint('Server', 'port'),
+        log_level="warning",
+        reload_includes=["*.html", '*.js', '*.css'],
+        reload=True
+    )
 
 
 if __name__ == "__main__":
