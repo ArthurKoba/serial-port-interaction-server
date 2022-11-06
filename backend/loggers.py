3ae05fb3c3a8b42eb5003b4cd844da86771b1289
Arthur Koba <caprimores@gmail.com>
2022-11-06 06:40:14 +0300

    Add loggers

---
+++ backend/loggers.py
@@ -0,0 +1,33 @@
+import logging
+from colorlog import ColoredFormatter
+
+from .settings import get_config
+
+config = get_config()
+
+logging_level = config.get('Server', 'log_level').upper()
+string_format = "%(log_color)s%(levelname)-8.8s|%(asctime)s| %(name)-10s| - %(message)s%(reset)s"
+date_format = "%Y-%m-%d %H:%M:%S"
+
+formatter = ColoredFormatter(fmt=string_format, datefmt=date_format)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+logging.basicConfig(
+    level=logging_level,
+    format=string_format,
+    datefmt=date_format,
+    handlers=[handler]
+)
+
+logging.getLogger("asyncio").setLevel(logging.INFO)
+
+main_logger = logging.getLogger("main")
+serial_logger = logging.getLogger("serial")
+
+serial_logger.setLevel(logging.INFO)
+
+__all__ = [
+    "main_logger",
+    "serial_logger"
+]
