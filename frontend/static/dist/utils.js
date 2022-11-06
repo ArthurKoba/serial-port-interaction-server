8097983a5d469223f6ca12a81131889942073cf2
Arthur Koba <caprimores@gmail.com>
2022-11-06 06:50:34 +0300

    Add functionality to generator values

--- frontend/static/dist/utils.js
+++ frontend/static/dist/utils.js
@@ -1,32 +1,68 @@
-export class FrequrencyGenerator {
+export class FrequencyGenerator {
   constructor (length) {
       this._length = length
       this._labels = []
       this._values = []
       this._harmonics = []
+      this._noises = []
+      this._offset = 0
+      this._lowLimit = null
+      this._highLimit = null
   }
 
-  add_harmonic (mathFunc, frequrency, amplitude, offset = 0) {
-    this._harmonics.push({mathFunc, frequrency, amplitude, offset})
+  addOffset(value) {
+    this._offset = value
+  }
+
+  addHarmonic (mathFunc, frequency, amplitude) {
+    this._harmonics.push({mathFunc, frequency: frequency, amplitude: amplitude/2})
+  }
+
+  addNoise (amplitude) {
+    this._noises.push(amplitude)
+  }
+
+  addLowLimit (value) {
+    this._lowLimit = value
+  }
+
+  addHighLimit (value) {
+    this._highLimit = value
   }
 
   generate (valueToResize = null) {
     if (!this._harmonics.length)
       throw new Error('Before need add_harmonic(). Template: object.add_harmonic(Math.sin, 1, 128)')
+
     for (let i = 0; i < this._length; i++) {
       this._values[i] = 0
       this._labels.push(i+1)
     }
 
-    for (let i = 0; i < this._length; i++) {
-        for (let harmonic of this._harmonics) {
-          this._values[i] += Math.round(
-              harmonic.mathFunc(i/harmonic.frequrency) *
-              harmonic.amplitude/2 + harmonic.amplitude/2 + harmonic.offset
-          )
-        }
+    if (this._offset) {
+        let sumAmplitudes = 0
+        for (let harmonic of this._harmonics) sumAmplitudes += harmonic.amplitude
+        this._offset -= sumAmplitudes
     }
 
+    for (let i = 0; i < this._length; i++) {
+        for (let harmonic of this._harmonics) {
+          this._values[i] += harmonic.mathFunc(i/harmonic.frequency) *
+              harmonic.amplitude + harmonic.amplitude
+        }
+
+        for (let noiseLevel of this._noises)
+          this._values[i] += Math.random() * noiseLevel - noiseLevel
+
+        this._values[i] += this._offset
+
+        this._values[i] = Math.round(this._values[i])
+
+        if (this._lowLimit && this._values[i] < this._lowLimit)
+            this._values[i] = this._lowLimit
+        if (this._highLimit && this._values[i] > this._highLimit)
+            this._values[i] = this._highLimit
+    }
     if (valueToResize) {
 
       let minValue = Math.min.apply(null, this._values)
